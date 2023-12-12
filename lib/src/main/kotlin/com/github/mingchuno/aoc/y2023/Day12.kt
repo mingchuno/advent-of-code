12f6fbfc999fe09082ea0154ee7b66453f5585af
mingchuno <mingchuno@gmail.com>
2023-12-13 00:30:33 +0800

    2023 day 12 complete

--- lib/src/main/kotlin/com/github/mingchuno/aoc/y2023/Day12.kt
+++ lib/src/main/kotlin/com/github/mingchuno/aoc/y2023/Day12.kt
@@ -2,63 +2,21 @@ package com.github.mingchuno.aoc.y2023
 
 import com.github.mingchuno.aoc.interfaceing.Problem
 import com.github.mingchuno.aoc.utils.readFileFromResource
-import kotlinx.coroutines.runBlocking
 
-class SpringConfig {
+class SpringConfig(private val scale: Int) {
 
     private val pCache = mutableMapOf<String, Long>()
-    private val sCache = mutableMapOf<String, Long>()
-
-    fun search(springs: List<Char>, damagedCount: List<Int>): Long {
-        return possibleConfig(springs, damagedCount).also { println(it) }
-    }
 
     private fun toKey(spring: String, damagedCount: List<Int>): String =
         spring + damagedCount.joinToString(",")
 
-    private fun toKey(springs: List<String>, damagedCount: List<Int>): String =
-        springs.joinToString("-") + damagedCount.joinToString(",")
-
-    fun part2(springs: String, damaged: String): Long {
-        val scale = 5
+    fun search(springs: String, damaged: String): Long {
         val d = damaged.split(",").map { it.toInt() }
         val fullDamaged = List(scale) { d }.flatten()
-        return possibleConfigM(List(scale) { springs }.joinToString("?").toList(), fullDamaged)
+        return possibleConfigMemo(List(scale) { springs }.joinToString("?").toList(), fullDamaged)
     }
 
-    fun search2(springs: List<String>, damagedCount: List<Int>): Long {
-        if (springs.isEmpty() && damagedCount.isEmpty()) {
-            return 1
-        }
-        if (springs.isEmpty() || damagedCount.isEmpty()) {
-            return 0
-        }
-        val sKey = toKey(springs, damagedCount)
-        if (sCache.containsKey(sKey)) {
-            return sCache[sKey]!!
-        }
-        val firstSpringGroup = springs.first()
-        val tailSpringGroup = springs.drop(1)
-        val firstSpringGroupSize = firstSpringGroup.length
-        val firstGroupMin = firstSpringGroup.count { it == '#' }
-        val runningSum = damagedCount.runningReduce { acc, i -> acc + i }
-        val endIdx = runningSum.indexOfFirst { firstSpringGroupSize <= it }
-        val startIdx = runningSum.indexOfFirst { firstGroupMin > it }
-        val trueEnd = if (endIdx < 0) damagedCount.size - 1 else endIdx
-        val trueStart = if (startIdx >= 0) startIdx else 0
-        var sum = 0L
-        for (i in trueStart..trueEnd + 1) {
-            val headDamage = damagedCount.take(i)
-            val tailDamage = damagedCount.drop(i)
-            sum +=
-                possibleConfig(firstSpringGroup.toList(), headDamage) *
-                    search2(tailSpringGroup, tailDamage)
-        }
-        sCache[sKey] = sum
-        return sum
-    }
-
-    private fun possibleConfigM(springs: List<Char>, damagedCount: List<Int>): Long {
+    private fun possibleConfigMemo(springs: List<Char>, damagedCount: List<Int>): Long {
         val key = toKey(springs.joinToString(""), damagedCount)
         return pCache[key] ?: possibleConfig(springs, damagedCount).also { pCache[key] = it }
     }
@@ -81,18 +39,20 @@ class SpringConfig {
         if (springs.size < damagedCount.sum()) {
             return 0
         }
-
         return when (springs.first()) {
-            '.' -> possibleConfigM(springs.drop(1), damagedCount)
+            '.' -> possibleConfigMemo(springs.drop(1), damagedCount)
             '#' -> {
                 if (springs.take(firstDamage).all { it == '#' || it == '?' }) {
                     if (springs.size == firstDamage) {
-                        possibleConfigM(listOf(), damagedCount.drop(1))
+                        possibleConfigMemo(listOf(), damagedCount.drop(1))
                     } else {
                         when (springs[firstDamage]) {
                             '#' -> 0
                             else ->
-                                possibleConfigM(springs.drop(firstDamage + 1), damagedCount.drop(1))
+                                possibleConfigMemo(
+                                    springs.drop(firstDamage + 1),
+                                    damagedCount.drop(1)
+                                )
                         }
                     }
                 } else {
@@ -100,8 +60,8 @@ class SpringConfig {
                 }
             }
             '?' -> {
-                possibleConfigM(springs.replaceFirst('.'), damagedCount) +
-                    possibleConfigM(springs.replaceFirst('#'), damagedCount)
+                possibleConfigMemo(springs.replaceFirst('.'), damagedCount) +
+                    possibleConfigMemo(springs.replaceFirst('#'), damagedCount)
             }
             else -> throw Exception("This should not happens")
         }
@@ -112,24 +72,18 @@ class SpringConfig {
 
 object Day12 : Problem<Long> {
     override fun computePart1(inputFile: String): Long {
-        val inputs = inputFile.readFileFromResource()
-        return inputs
-            .map { line ->
-                val (springs, damaged) = line.split(" ")
-                SpringConfig().search(springs.toList(), damaged.split(",").map { it.toInt() })
-            }
-            .sum()
+        return compute(inputFile, 1)
     }
 
     override fun computePart2(inputFile: String): Long {
+        return compute(inputFile, 5)
+    }
+
+    private fun compute(inputFile: String, scale: Int): Long {
         val inputs = inputFile.readFileFromResource()
-        return runBlocking {
-            inputs
-                .map { line ->
-                    val (springs, damaged) = line.split(" ")
-                    SpringConfig().part2(springs, damaged)
-                }
-                .sum()
+        return inputs.sumOf { line ->
+            val (springs, damaged) = line.split(" ")
+            SpringConfig(scale).search(springs, damaged)
         }
     }
 }
--- lib/src/test/kotlin/com/github/mingchuno/aoc/y2023/Day12Test.kt
+++ lib/src/test/kotlin/com/github/mingchuno/aoc/y2023/Day12Test.kt
@@ -8,56 +8,46 @@ private data class SpringSpec(val springs: String, val damagedCount: List<Int>, 
 
 class Day12Test :
     DescribeSpec({
-        describe("possible config") {
-            context("possibleConfig") {
-                withData(
-                    nameFn = {
-                        "${it.springs} ${it.damagedCount} should have ${it.expected} possible config"
-                    },
-                    SpringSpec("#.#.###", listOf(1, 1, 3), 1),
-                    SpringSpec("???.###", listOf(1, 1, 3), 1),
-                    SpringSpec(".??..??...?##.", listOf(1, 1, 3), 4),
-                    SpringSpec("?#?#?#?#?#?#?#?", listOf(1, 3, 1, 6), 1),
-                    SpringSpec("????.#...#...", listOf(4, 1, 1), 1),
-                    SpringSpec("????.######..#####", listOf(1, 6, 5), 4),
-                    SpringSpec("?###????????", listOf(3, 2, 1), 10),
-                ) { (springs, damagedCount, expected) ->
-                    SpringConfig().search(springs.toList(), damagedCount).shouldBe(expected)
-                }
+        describe("line example - scale=1") {
+            withData(
+                nameFn = {
+                    "${it.springs} ${it.damagedCount} should have ${it.expected} possible config"
+                },
+                SpringSpec("#.#.###", listOf(1, 1, 3), 1),
+                SpringSpec("???.###", listOf(1, 1, 3), 1),
+                SpringSpec(".??..??...?##.", listOf(1, 1, 3), 4),
+                SpringSpec("?#?#?#?#?#?#?#?", listOf(1, 3, 1, 6), 1),
+                SpringSpec("????.#...#...", listOf(4, 1, 1), 1),
+                SpringSpec("????.######..#####", listOf(1, 6, 5), 4),
+                SpringSpec("?###????????", listOf(3, 2, 1), 10),
+                SpringSpec("??#??????#..?????", listOf(9, 2, 1), 3),
+                SpringSpec("????.????.", listOf(1, 1), 22),
+                SpringSpec("??.??.?##", listOf(1, 1, 3), 4),
+                SpringSpec("?????", listOf(4), 2),
+            ) { (springs, damagedCount, expected) ->
+                SpringConfig(1).search(springs, damagedCount.joinToString(",")).shouldBe(expected)
             }
         }
 
-        describe("possible config part 2") {
-            context("possibleConfig") {
-                withData(
-                    nameFn = {
-                        "${it.springs} ${it.damagedCount} should have ${it.expected} possible config"
-                    },
-                    SpringSpec("???.###", listOf(1, 1, 3), 1),
-                    SpringSpec(".??..??...?##.", listOf(1, 1, 3), 16384),
-                    SpringSpec("?#?#?#?#?#?#?#?", listOf(1, 3, 1, 6), 1),
-                    SpringSpec("????.#...#...", listOf(4, 1, 1), 16),
-                    SpringSpec("????.######..#####.", listOf(1, 6, 5), 2500),
-                    SpringSpec("?###????????", listOf(3, 2, 1), 506250),
-                ) { (springs, damagedCount, expected) ->
-                    SpringConfig().part2(springs, damagedCount.joinToString(",")).shouldBe(expected)
-                }
+        describe("line example - scale=5") {
+            withData(
+                nameFn = {
+                    "${it.springs} ${it.damagedCount} should have ${it.expected} possible config"
+                },
+                SpringSpec("???.###", listOf(1, 1, 3), 1),
+                SpringSpec(".??..??...?##.", listOf(1, 1, 3), 16384),
+                SpringSpec("?#?#?#?#?#?#?#?", listOf(1, 3, 1, 6), 1),
+                SpringSpec("????.#...#...", listOf(4, 1, 1), 16),
+                SpringSpec("????.######..#####.", listOf(1, 6, 5), 2500),
+                SpringSpec("?###????????", listOf(3, 2, 1), 506250),
+                SpringSpec("###", listOf(3), 1),
+                SpringSpec("???", listOf(1, 1), 1),
+                SpringSpec("???.###", listOf(1, 1, 3), 1),
+                SpringSpec("??#??????#..?????", listOf(9, 2, 1), 3888),
+                SpringSpec("????.????.", listOf(1, 1), 345615702),
+            ) { (springs, damagedCount, expected) ->
+                SpringConfig(5).search(springs, damagedCount.joinToString(",")).shouldBe(expected)
             }
-        }
-
-        describe("search2") {
-            it("atom case 1") { SpringConfig().search2(listOf("###"), listOf(3)).shouldBe(1) }
-            it("atom case 2") { SpringConfig().search2(listOf("???"), listOf(1, 1)).shouldBe(1) }
-            it("atom case 3") {
-                SpringConfig().search2(listOf("???", "###"), listOf(1, 1, 3)).shouldBe(1)
-            }
-            it("atom case 4") {
-                SpringConfig().search2(listOf("??", "??", "?##"), listOf(1, 1, 3)).shouldBe(4)
-            }
-            it("atom case 5") {
-                SpringConfig().search2(listOf("?###????????"), listOf(3, 2, 1)).shouldBe(10)
-            }
-            it("atom case 6") { SpringConfig().search2(listOf("?????"), listOf(4)).shouldBe(2) }
         }
 
         describe("part 1") {
@@ -70,31 +60,5 @@ class Day12Test :
             it("example case") { Day12.computePart2("2023/day12-example.txt").shouldBe(525152) }
 
             it("real case") { Day12.computePart2("2023/day12-real.txt").shouldBe(6720660274964) }
-        }
-
-        describe("part 2 test") {
-            context("part2") {
-                withData(
-                    nameFn = {
-                        "${it.springs} ${it.damagedCount} should have ${it.expected} possible config"
-                    },
-                    SpringSpec("??#??????#..?????", listOf(9, 2, 1), 3888),
-                    SpringSpec("????.????.", listOf(1, 1), 345615702),
-                ) { (springs, damagedCount, expected) ->
-                    SpringConfig().part2(springs, damagedCount.joinToString(",")).shouldBe(expected)
-                }
-            }
-
-            context("possibleConfig") {
-                withData(
-                    nameFn = {
-                        "${it.springs} ${it.damagedCount} should have ${it.expected} possible config"
-                    },
-                    SpringSpec("??#??????#..?????", listOf(9, 2, 1), 3),
-                    SpringSpec("????.????.", listOf(1, 1), 22),
-                ) { (springs, damagedCount, expected) ->
-                    SpringConfig().search(springs.toList(), damagedCount).shouldBe(expected)
-                }
-            }
         }
     })
