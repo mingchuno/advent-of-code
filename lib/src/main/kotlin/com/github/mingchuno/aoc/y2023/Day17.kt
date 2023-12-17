363c65230e3666bb6500b89e7c677c1bed7ed6e8
mingchuno <mingchuno@gmail.com>
2023-12-18 00:41:30 +0800

    2023 day 17 part 1 stucks

--- lib/src/main/kotlin/com/github/mingchuno/aoc/y2023/Day17.kt
+++ lib/src/main/kotlin/com/github/mingchuno/aoc/y2023/Day17.kt
@@ -3,6 +3,7 @@ package com.github.mingchuno.aoc.y2023
 import com.github.mingchuno.aoc.interfaceing.Problem
 import com.github.mingchuno.aoc.utils.Coord
 import com.github.mingchuno.aoc.utils.readFileFromResource
+import kotlin.math.abs
 
 object Day17 : Problem<Int> {
     override fun computePart1(inputFile: String): Int {
@@ -18,6 +19,8 @@ object Day17 : Problem<Int> {
 
 private class Dijkstra(private val graph: List<List<Int>>) {
     private val SIZE = graph.size
+
+    private val debug = true
 
     private val visited: List<MutableList<Boolean>> = List(SIZE) { MutableList(SIZE) { false } }
     private val prevNode: MutableMap<Coord, Coord> = mutableMapOf()
@@ -38,7 +41,7 @@ private class Dijkstra(private val graph: List<List<Int>>) {
         val prev = prevNode[x to y]
         if (prev != null) {
             val (px, py) = prev
-            println("x=$px;y=$py")
+            println("x=$px;y=$py;cost=${graph[py][px]}")
             printPath(px, py)
         }
     }
@@ -49,11 +52,11 @@ private class Dijkstra(private val graph: List<List<Int>>) {
             val (nX, nY) = coord
             costs.remove(coord)
             if (nX + 1 == SIZE && nY + 1 == SIZE) {
-                printPath()
+                if (debug) printPath()
                 return cost
             }
             val neighbors = findNeighbor(nX, nY)
-            println("nX=$nX;nY=$nY;neighbors=$neighbors")
+            if (debug) println("nX=$nX;nY=$nY;neighbors=$neighbors")
             neighbors.forEach { (x, y) ->
                 val newCost = graph[y][x] + cost
                 val oldCost = costs[x to y]!!
@@ -68,23 +71,27 @@ private class Dijkstra(private val graph: List<List<Int>>) {
     }
 
     private fun findNeighbor(x: Int, y: Int): List<Coord> {
+        val maxSameSteps = 3
         val neighbor = mutableListOf<Coord>()
-        //        val lastPrev = prevNode[x to y]
-        //        val secondLastPrev = lastPrev?.let { prevNode[it] }
-        //        val (sameX, sameY) =
-        //            if (secondLastPrev == null) {
-        //                false to false
-        //            } else {
-        //                val (px, py) = lastPrev
-        //                val (ppx, ppy) = secondLastPrev
-        //                (x == px && x == ppx) to (y == py && y == ppy)
-        //            }
-        val (sameX, sameY) = false to false
-        if (validCoord(x + 1, y) && !sameX) neighbor.add(x + 1 to y)
-        if (validCoord(x - 1, y) && !sameX) neighbor.add(x - 1 to y)
-        if (validCoord(x, y + 1) && !sameY) neighbor.add(x to y + 1)
-        if (validCoord(x, y - 1) && !sameY) neighbor.add(x to y - 1)
+        val back3Steps = backtrack(x to y, maxSameSteps)
+        if (debug) println("x=$x;y=$y;back3Steps=$back3Steps")
+        val (invalidXDirection, invalidYDirection) =
+            if (back3Steps == null) {
+                false to false
+            } else {
+                val (px, py) = back3Steps
+                (abs(px - x) == maxSameSteps && py == y) to (abs(py - y) == maxSameSteps && px == x)
+            }
+        if (validCoord(x + 1, y) && !invalidXDirection) neighbor.add(x + 1 to y)
+        if (validCoord(x - 1, y) && !invalidXDirection) neighbor.add(x - 1 to y)
+        if (validCoord(x, y + 1) && !invalidYDirection) neighbor.add(x to y + 1)
+        if (validCoord(x, y - 1) && !invalidYDirection) neighbor.add(x to y - 1)
         return neighbor
+    }
+
+    private fun backtrack(from: Coord, count: Int): Coord? {
+        if (count == 0) return from
+        return prevNode[from]?.let { backtrack(it, count - 1) }
     }
 
     private fun validCoord(x: Int, y: Int): Boolean =
--- lib/src/test/kotlin/com/github/mingchuno/aoc/y2023/Day17Test.kt
+++ lib/src/test/kotlin/com/github/mingchuno/aoc/y2023/Day17Test.kt
@@ -7,8 +7,12 @@ class Day17Test :
     DescribeSpec({
         describe("part 1") {
             it("example case") { Day17.computePart1("2023/day17-example.txt").shouldBe(102) }
+            it("example case 2") { Day17.computePart1("2023/day17-example2.txt").shouldBe(14) }
+            it("example case 3") { Day17.computePart1("2023/day17-example3.txt").shouldBe(13) }
+            it("example case 4") { Day17.computePart1("2023/day17-example4.txt").shouldBe(13) }
+            it("example case 5") { Day17.computePart1("2023/day17-example5.txt").shouldBe(29) }
 
-            it("real case") { Day17.computePart1("2023/day17-real.txt").shouldBe(0) }
+            //            it("real case") { Day17.computePart1("2023/day17-real.txt").shouldBe(0) }
         }
 
         describe("part 2") {
