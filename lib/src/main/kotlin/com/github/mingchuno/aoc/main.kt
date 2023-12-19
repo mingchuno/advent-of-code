77cb5e23f97d6c65533d468e93b3257beec09667
mingchuno <mingchuno@gmail.com>
2023-12-19 17:34:41 +0800

    2023 day 19 part 2 wip

--- lib/src/main/kotlin/com/github/mingchuno/aoc/main.kt
+++ lib/src/main/kotlin/com/github/mingchuno/aoc/main.kt
@@ -1,12 +1,14 @@
 package com.github.mingchuno.aoc
 
-import com.github.mingchuno.aoc.y2023.Day17
+import com.github.mingchuno.aoc.y2023.Day19
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     runBlocking {
-        val ans = Day17.computePart2("2023/day17-real.txt")
+        val ans = Day19.computePart2("2023/day19-example.txt")
         println("Ans=$ans")
     }
 }
 // 23147 17873 20803 15529 19631 17287
+// 167409079868000
+// 266518932300000
--- lib/src/main/kotlin/com/github/mingchuno/aoc/y2023/Day19.kt
+++ lib/src/main/kotlin/com/github/mingchuno/aoc/y2023/Day19.kt
@@ -3,46 +3,194 @@ package com.github.mingchuno.aoc.y2023
 import com.github.mingchuno.aoc.interfaceing.Problem
 import com.github.mingchuno.aoc.utils.readFileFromResource
 
-object Day19 : Problem<Int> {
-    override fun computePart1(inputFile: String): Int {
+object Day19 : Problem<Long> {
+    override fun computePart1(inputFile: String): Long {
         val inputs = inputFile.readFileFromResource()
         val (workflows, parts) = parseInputs(inputs)
         return parts
             .filter { part -> part.passWorkflows(workflows) == EndState.ACCEPTED }
             .sumOf { it.score() }
+            .toLong()
     }
 
-    override fun computePart2(inputFile: String): Int {
+    override fun computePart2(inputFile: String): Long {
         val inputs = inputFile.readFileFromResource()
-        TODO("Not yet implemented")
+        val (workflows, _) = parseInputs(inputs)
+        val ruless = revereRulesTracking(workflows)
+        return ruless.sumOf { it.computeRange() }
     }
 }
 
 private typealias Workflows = Map<String, Workflow>
+
+private data class BackTrackingRule(val rule: RuleBase, var truthful: Boolean)
+
+private fun revereRulesTracking(workflows: Workflows): List<List<BackTrackingRule>> {
+    val reversedWorkflows = workflows.mapValues { (k, v) -> v.copy(rules = v.rules.reversed()) }
+    val backtrackingIndex = findKeyAndHitIndex(reversedWorkflows)
+
+    //    reversedWorkflows.onEach { println(it) }
+    //    backtrackingIndex.onEach { println(it) }
+
+    val results = mutableListOf<List<BackTrackingRule>>()
+    reversedWorkflows.forEach { (key, workflow) ->
+        workflow.rules.forEachIndexed { idx, rule ->
+            if (rule.target == "A") {
+                results.add(
+                    buildBackTrackingList(
+                            reversedWorkflows,
+                            backtrackingIndex,
+                            currentKey = key,
+                            hitIdx = idx
+                        )
+                        // First item in the list (last item in the rules) should be true
+                        .also { it[0].truthful = true }
+                )
+            }
+        }
+    }
+    results.onEach { println(it) }
+    return results
+}
+
+private val MIN = 1
+private val MAX = 4000
+
+private fun initRange(): MutableMap<String, IntRange> =
+    mutableMapOf(
+        "x" to MIN..MAX,
+        "m" to MIN..MAX,
+        "a" to MIN..MAX,
+        "s" to MIN..MAX,
+    )
+
+private fun List<BackTrackingRule>.computeRange(): Long {
+    val finalRange =
+        this.fold(initRange()) { prevRange, brule ->
+            val rule = brule.rule
+            val truthful = brule.truthful
+            when (rule) {
+                is DefaultRule -> prevRange
+                is Rule -> {
+                    val oldRange = prevRange[rule.cat]!!
+                    when (rule.operator) {
+                        Operator.gt -> {
+                            if (truthful) {
+                                if (rule.value < oldRange.first) {
+                                    prevRange
+                                } else if (oldRange.last < rule.value) {
+                                    prevRange[rule.cat] = MAX..MIN
+                                    prevRange
+                                } else {
+                                    prevRange[rule.cat] = rule.value..oldRange.last
+                                    prevRange
+                                }
+                            } else {
+                                if (rule.value < oldRange.first) {
+                                    prevRange[rule.cat] = MAX..MIN
+                                    prevRange
+                                } else if (oldRange.last < rule.value) {
+                                    prevRange
+                                } else {
+                                    prevRange[rule.cat] = oldRange.first..rule.value
+                                    prevRange
+                                }
+                            }
+                        }
+                        Operator.lt -> {
+                            if (truthful) {
+                                if (rule.value < oldRange.first) {
+                                    prevRange[rule.cat] = MAX..MIN
+                                    prevRange
+                                } else if (oldRange.last < rule.value) {
+                                    prevRange
+                                } else {
+                                    prevRange[rule.cat] = oldRange.first..rule.value
+                                    prevRange
+                                }
+                            } else {
+
+                                if (rule.value < oldRange.first) {
+                                    prevRange
+                                } else if (oldRange.last < rule.value) {
+                                    prevRange[rule.cat] = MAX..MIN
+                                    prevRange
+                                } else {
+                                    prevRange[rule.cat] = rule.value..oldRange.last
+                                    prevRange
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    return finalRange.values.map { it.count().toLong() }.reduce { acc, i -> acc * i }
+}
+
+private fun findKeyAndHitIndex(
+    workflows: Workflows, // reversed!
+): Map<String, List<Pair<String, Int>>> {
+    return workflows.keys.associateWith { currentKey ->
+        (workflows.flatMap { (key, wf) ->
+            wf.rules
+                .withIndex()
+                .filter { (_, rule) -> rule.target == currentKey }
+                .map { (idx, _) -> key to idx }
+        })
+    }
+}
+
+private fun buildBackTrackingList(
+    workflows: Workflows, // reversed!
+    backtrackingIndex: Map<String, List<Pair<String, Int>>>,
+    currentKey: String,
+    hitIdx: Int
+): List<BackTrackingRule> {
+    val currentWorkflow = workflows[currentKey]!!
+    val size = currentWorkflow.rules.size
+    val xs = (hitIdx ..< size).map { BackTrackingRule(currentWorkflow.rules[it], false) }
+    val ys =
+        if (currentKey == "in") listOf()
+        else
+            (backtrackingIndex[currentKey] ?: listOf())
+                .map { (nextKey, nextIdx) ->
+                    buildBackTrackingList(workflows, backtrackingIndex, nextKey, nextIdx)
+                }
+                .flatten()
+    return xs + ys
+}
 
 private fun MPart.passWorkflows(workflows: Workflows, nextWorkflow: String = "in"): EndState {
     val w = workflows[nextWorkflow]!!
     var nextTarget: String? = null
     loop@ for (rule in w.rules) {
         val pass =
-            when (rule.cat to rule.operator) {
-                "x" to Operator.gt -> this.x > rule.value
-                "x" to Operator.lt -> this.x < rule.value
-                "m" to Operator.gt -> this.m > rule.value
-                "m" to Operator.lt -> this.m < rule.value
-                "a" to Operator.gt -> this.a > rule.value
-                "a" to Operator.lt -> this.a < rule.value
-                "s" to Operator.gt -> this.s > rule.value
-                "s" to Operator.lt -> this.s < rule.value
-                else -> false
+            when (rule) {
+                is Rule -> {
+                    when (rule.cat to rule.operator) {
+                        "x" to Operator.gt -> this.x > rule.value
+                        "x" to Operator.lt -> this.x < rule.value
+                        "m" to Operator.gt -> this.m > rule.value
+                        "m" to Operator.lt -> this.m < rule.value
+                        "a" to Operator.gt -> this.a > rule.value
+                        "a" to Operator.lt -> this.a < rule.value
+                        "s" to Operator.gt -> this.s > rule.value
+                        "s" to Operator.lt -> this.s < rule.value
+                        else -> false
+                    }
+                }
+                is DefaultRule -> {
+                    true
+                }
             }
         if (pass) {
             nextTarget = rule.target
             break@loop
         }
     }
-    nextTarget = nextTarget ?: w.defaultTarget
     return when (nextTarget) {
+        null -> throw Exception("Should not happens")
         "A" -> EndState.ACCEPTED
         "R" -> EndState.REJECTED
         else -> this.passWorkflows(workflows, nextTarget)
@@ -59,7 +207,7 @@ private fun parseInputs(inputs: List<String>): Pair<Workflows, List<MPart>> {
 private fun List<Workflow>.toWorkflowMap(): Workflows = associateBy { it.key }
 
 private val workflowRegex = """(.*)\{(.*)\}""".toRegex()
-private val ruleRgex = """([xmas])([>|<])(\d+):(.+)""".toRegex()
+private val ruleRegex = """([xmas])([>|<])(\d+):(.+)""".toRegex()
 
 private fun String.parseWorkflow(): Workflow {
     val (key, workflowsStr) = workflowRegex.find(this)?.groupValues?.drop(1)!!
@@ -67,7 +215,7 @@ private fun String.parseWorkflow(): Workflow {
     val defaultTarget = ws.last()
     val rules =
         ws.dropLast(1).map {
-            val (cat, operator, value, target) = ruleRgex.find(it)?.groupValues?.drop(1)!!
+            val (cat, operator, value, target) = ruleRegex.find(it)?.groupValues?.drop(1)!!
             val op =
                 when (operator) {
                     ">" -> Operator.gt
@@ -77,7 +225,7 @@ private fun String.parseWorkflow(): Workflow {
             Rule(cat, op, value.toInt(), target)
         }
 
-    return Workflow(key = key, rules = rules, defaultTarget = defaultTarget)
+    return Workflow(key = key, rules = rules + DefaultRule(defaultTarget))
 }
 
 private val partRegexp = """\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)\}""".toRegex()
@@ -91,14 +239,20 @@ private data class MPart(val x: Int, val m: Int, val a: Int, val s: Int) {
     fun score(): Int = x + m + a + s
 }
 
+sealed interface RuleBase {
+    val target: String
+}
+
 private data class Rule(
     val cat: String,
     val operator: Operator,
     val value: Int,
-    val target: String
-)
+    override val target: String
+) : RuleBase
 
-private data class Workflow(val key: String, val rules: List<Rule>, val defaultTarget: String)
+private data class DefaultRule(override val target: String) : RuleBase
+
+private data class Workflow(val key: String, val rules: List<RuleBase>)
 
 private enum class EndState {
     ACCEPTED,
