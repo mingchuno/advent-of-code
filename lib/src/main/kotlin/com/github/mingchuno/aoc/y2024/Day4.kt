66d7ebec2dda3f995af59c811965c3318ab742ab
mingchuno <mingchuno@gmail.com>
2024-12-04 17:20:42 +0000

    2024 day 4 part 1

---
+++ lib/src/main/kotlin/com/github/mingchuno/aoc/y2024/Day4.kt
@@ -0,0 +1,95 @@
+package com.github.mingchuno.aoc.y2024
+
+import com.github.mingchuno.aoc.interfaceing.Problem
+import com.github.mingchuno.aoc.utils.readFileFromResource
+import com.github.mingchuno.aoc.utils.to2DChars
+import com.github.mingchuno.aoc.utils.transpose
+
+object Day4 : Problem<Int> {
+
+    private fun List<Char>.isXmas(): Boolean = this.joinToString("") == "XMAS"
+
+    override fun computePart1(inputFile: String): Int {
+        val inputs = inputFile.readFileFromResource().to2DChars()
+        val inputsT = inputs.transpose()
+        val Y = inputs.size
+        val X = inputs.first().size
+        var count = 0
+        inputs.forEachIndexed { y, row ->
+            row.forEachIndexed { x, c ->
+                if (c == 'X') {
+                    // right direction
+                    if (row.subList(x, (x + 4).coerceAtMost(X)).isXmas()) {
+                        count++
+                    }
+                    // left direction
+                    if (row.slice((x - 3).coerceAtLeast(0)..x).reversed().isXmas()) {
+                        count++
+                    }
+                    // down direction
+                    if (inputsT[x].subList(y, (y + 4).coerceAtMost(Y)).isXmas()) {
+                        count++
+                    }
+                    // up direction
+                    if (inputsT[x].slice((y - 3).coerceAtLeast(0)..y).reversed().isXmas()) {
+                        count++
+                    }
+                    // NE direction
+                    if (
+                        listOfNotNull(
+                                'X',
+                                inputs.getOrNull(y - 1)?.getOrNull(x + 1),
+                                inputs.getOrNull(y - 2)?.getOrNull(x + 2),
+                                inputs.getOrNull(y - 3)?.getOrNull(x + 3),
+                            )
+                            .isXmas()
+                    ) {
+                        count++
+                    }
+                    // SE direction
+                    if (
+                        listOfNotNull(
+                                'X',
+                                inputs.getOrNull(y + 1)?.getOrNull(x + 1),
+                                inputs.getOrNull(y + 2)?.getOrNull(x + 2),
+                                inputs.getOrNull(y + 3)?.getOrNull(x + 3),
+                            )
+                            .isXmas()
+                    ) {
+                        count++
+                    }
+                    // SW direction
+                    if (
+                        listOfNotNull(
+                                'X',
+                                inputs.getOrNull(y + 1)?.getOrNull(x - 1),
+                                inputs.getOrNull(y + 2)?.getOrNull(x - 2),
+                                inputs.getOrNull(y + 3)?.getOrNull(x - 3),
+                            )
+                            .isXmas()
+                    ) {
+                        count++
+                    }
+                    // NW direction
+                    if (
+                        listOfNotNull(
+                                'X',
+                                inputs.getOrNull(y - 1)?.getOrNull(x - 1),
+                                inputs.getOrNull(y - 2)?.getOrNull(x - 2),
+                                inputs.getOrNull(y - 3)?.getOrNull(x - 3),
+                            )
+                            .isXmas()
+                    ) {
+                        count++
+                    }
+                }
+            }
+        }
+        return count
+    }
+
+    override fun computePart2(inputFile: String): Int {
+        val inputs = inputFile.readFileFromResource()
+        return 0
+    }
+}
---
+++ lib/src/test/kotlin/com/github/mingchuno/aoc/y2024/Day4Test.kt
@@ -0,0 +1,19 @@
+package com.github.mingchuno.aoc.y2024
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class Day4Test :
+    DescribeSpec({
+        describe("part 1") {
+            it("example case") { Day4.computePart1("2024/day4_example.txt").shouldBe(18) }
+
+            it("real case") { Day4.computePart1("2024/day4_input.txt").shouldBe(2517) }
+        }
+
+        describe("part 2") {
+            it("example case") { Day4.computePart2("2024/day4_example_2.txt").shouldBe(48) }
+
+            it("real case") { Day4.computePart2("2024/day4_input.txt").shouldBe(102467299) }
+        }
+    })
