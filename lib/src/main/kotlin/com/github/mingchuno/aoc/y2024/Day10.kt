ab9a64fd834bc7a0e63bf7503819c6be59b2074e
mingchuno <mingchuno@gmail.com>
2024-12-10 13:59:26 +0000

    refactor 2024 day 10

--- lib/src/main/kotlin/com/github/mingchuno/aoc/y2024/Day10.kt
+++ lib/src/main/kotlin/com/github/mingchuno/aoc/y2024/Day10.kt
@@ -8,14 +8,14 @@ object Day10 : Problem<Int> {
     override fun computePart1(inputFile: String): Int {
         val input = inputFile.readFileFromResource().to2DChars()
         val maze = input.toIntMaze()
-        val c = ComputePart1(maze)
+        val c = Compute(maze)
         return c.computeScore()
     }
 
-    private class ComputePart1(val maze: Maze<Int>) {
-        fun dfs(pos: Coord, height: Int): Set<Coord> {
+    private class Compute(val maze: Maze<Int>) {
+        fun dfs(pos: Coord, height: Int): List<Coord> {
             return if (height == 9) {
-                setOf(pos)
+                listOf(pos)
             } else {
                 val nextHeight = height + 1
                 maze.setCurrentPos(pos)
@@ -30,59 +30,33 @@ object Day10 : Problem<Int> {
             }
         }
 
-        private fun Maze.NextCell<Int>?.walk(nextHeight: Int): Set<Coord> =
-            this?.takeIf { it.cell == nextHeight }?.let { dfs(it.pos, nextHeight) } ?: setOf()
+        private fun Maze.NextCell<Int>?.walk(nextHeight: Int): List<Coord> =
+            this?.takeIf { it.cell == nextHeight }?.let { dfs(it.pos, nextHeight) } ?: listOf()
 
-        fun computeScore(): Int {
+        private fun compute(fn: (List<Coord>) -> Int): Int {
             return maze.inputs
                 .mapIndexed { y, rows ->
                     rows
                         .mapIndexed { x, height ->
-                            if (height == 0) dfs(Coord(x, y), height).size else 0
+                            if (height == 0) fn(dfs(Coord(x, y), height)) else 0
                         }
                         .sum()
                 }
                 .sum()
+        }
+
+        fun computeScore(): Int {
+            return compute { it.toSet().size }
+        }
+
+        fun computeRating(): Int {
+            return compute { it.size }
         }
     }
 
     override fun computePart2(inputFile: String): Int {
         val maze = inputFile.readFileFromResource().to2DChars().toIntMaze()
-        val c = ComputePart2(maze)
+        val c = Compute(maze)
         return c.computeRating()
-    }
-
-    private class ComputePart2(val maze: Maze<Int>) {
-        fun dfs(pos: Coord, height: Int): Int {
-            return if (height == 9) {
-                1
-            } else {
-                val nextHeight = height + 1
-                maze.setCurrentPos(pos)
-                val up = maze.walkUp(1)
-                val down = maze.walkDown(1)
-                val left = maze.walkLeft(1)
-                val right = maze.walkRight(1)
-                up.walk(nextHeight) +
-                    down.walk(nextHeight) +
-                    left.walk(nextHeight) +
-                    right.walk(nextHeight)
-            }
-        }
-
-        private fun Maze.NextCell<Int>?.walk(nextHeight: Int): Int =
-            this?.takeIf { it.cell == nextHeight }?.let { dfs(it.pos, nextHeight) } ?: 0
-
-        fun computeRating(): Int {
-            return maze.inputs
-                .mapIndexed { y, rows ->
-                    rows
-                        .mapIndexed { x, height ->
-                            if (height == 0) dfs(Coord(x, y), height) else 0
-                        }
-                        .sum()
-                }
-                .sum()
-        }
     }
 }
